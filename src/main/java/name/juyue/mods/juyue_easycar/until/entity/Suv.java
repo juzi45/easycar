a627860c8fde94e110aecbae0e1a1854a006a19f
juzi45 <JuziDroid@outlook.com>
2023-11-05 13:14:37 +0800

    new

--- src/main/java/name/juyue/mods/juyue_easycar/until/entity/Suv.java
+++ src/main/java/name/juyue/mods/juyue_easycar/until/entity/Suv.java
@@ -2,10 +2,13 @@ package name.juyue.mods.juyue_easycar.until.entity;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.vehicle.AbstractMinecartEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
@@ -14,6 +17,10 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
 
+
+
+
+//suv main suv的主体
 public abstract class Suv extends AbstractMinecartEntity{
 
 
@@ -24,14 +31,35 @@ public abstract class Suv extends AbstractMinecartEntity{
         return Items.MINECART;
     }
 }
-
-abstract class SuvEntity implements ModInitializer {
+//suv属性
+abstract class SuvEntity extends MobEntity implements ModInitializer {
 
      public static  final EntityType<Entity> SUV = Registry.register(
              Registries.ENTITY_TYPE,
              new Identifier("entitytest","suv"),
              FabricEntityTypeBuilder.create(SpawnGroup.CREATURE).dimensions(EntityDimensions.fixed(0.76f,0.75f)).build()
              );
+
+    protected SuvEntity(EntityType<? extends MobEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    //suv外观界定渲染器
+public class SuvRenderer extends MobEntityRenderer<SuvEntity,SuvModel>{
+    public  SuvRenderer(EntityRenderDispatcher entityRenderDispatcher){
+        super(entityRenderDispatcher,
+                new SuvModel(),
+                0.5f);
+    }
+    @Override
+    public Identifier getTexture(SuvEntity entity){
+        return new Identifier("entitytest","textuers/entity/suv/suv.png");
+    }
+
+}
+
+
+
 
     @Override
     public void onInitialize() {
---
+++ src/main/java/name/juyue/mods/juyue_easycar/until/entity/SuvModel.java
@@ -0,0 +1,6 @@
+package name.juyue.mods.juyue_easycar.until.entity;
+
+import net.minecraft.client.render.entity.model.EntityModel;
+
+public class SuvModel extends EntityModel<SuvEntity> {
+}
